478d6cba035ec342e943ebabd84083f6b1358310
Trai <107307068+mistertrai@users.noreply.github.com>
2024-04-28 01:32:21 +0700

    Update index.tsx - logo removed
    logo removed

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -331,9 +331,9 @@ const Welcome: FC<IWelcomeProps> = ({
               </div>
               : <div>
               </div>}
-            <a className='flex items-center pr-3 space-x-3' href="https://dify.ai/" target="_blank">
+            <a className='flex items-center pr-3 space-x-3' href="https://xlab.co.th/" target="_blank">
               <span className='uppercase'>{t('app.chat.powerBy')}</span>
-              <FootLogo />
+          
             </a>
           </div>
         )}
